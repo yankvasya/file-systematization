b602fde23c8ea4400dbe223ee6ca8e534d60aa38
yankvasya <yankvasya@gmail.com>
2022-02-20 22:50:10 +0300

    refactor: refactor & changed functions to sync

--- main.js
+++ main.js
@@ -11,44 +11,34 @@ const createDir = (directory) => {
 
   const isDirExist = fs.existsSync(absolutePath(directory));
 
-  if (!isDirExist) {
-    fs.mkdirSync(directory);
-  }
+  if (!isDirExist) fs.mkdirSync(directory);
 };
 
-const writeFile = async (file, directory, encoding = 'base64') => {
-  if (encoding === 'base64') {
-    file = Buffer.from(file, 'base64');
-  }
-  await fs.writeFile(directory, file, { encoding }, (err) => {
-    if (err) throw err;
-
-    console.log('file saved to', directory.red);
-  });
+const writeFile = (file, directory, encoding = 'base64') => {
+  fs.writeFileSync(directory, file, { encoding });
+  console.log('file saved to', directory.red);
 };
 
-const readFromDir = () => {
+const replaceFiles = () => {
   const names = fs.readdirSync(absolutePath(from));
   const files = names.map(name => absolutePath(path.join(from, name)));
 
   files.forEach((file, npx) => {
     const name = names[npx];
-    const dir = name[0].toUpperCase();
+    const firstLetter = name[0].toUpperCase();
+    const directory = path.join(to, firstLetter, name);
     // Создает директорию с названием первой буквы файлы
-    createDir(path.join(to, dir));
+    createDir(path.join(to, firstLetter));
 
-    fs.readFile(file, 'base64', async (error, data) => {
-      if (error) throw error;
-
-      await writeFile(data, path.join(to, dir, name));
-    });
+    const data = fs.readFileSync(file, 'base64');
+    writeFile(data, directory);
   });
 };
 
 const init = () => {
   createDir([from, to]);
 
-  readFromDir();
+  replaceFiles();
 };
 
 init();
