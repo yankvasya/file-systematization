d512ebd201c89e28410bbfa04fd10c8bb8503b95
yankvasya <yankvasya@gmail.com>
2022-02-20 22:52:03 +0300

    feat: isFromExists & sort files in dirs in from_dir

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ require('colors');
 
 const fs = require('fs');
 const path = require('path');
-const { from, to } = require('minimist')(process.argv.slice(2));
+const { from, to, remove } = require('minimist')(process.argv.slice(2));
 
 const absolutePath = (name) => path.join(__dirname, name);
 
@@ -14,17 +14,36 @@ const createDir = (directory) => {
   if (!isDirExist) fs.mkdirSync(directory);
 };
 
-const writeFile = (file, directory, encoding = 'base64') => {
-  fs.writeFileSync(directory, file, { encoding });
-  console.log('file saved to', directory.red);
+const isExist = (file) => fs.existsSync(absolutePath(file));
+
+const removeDir = (directory) => {
+  fs.rmdirSync(directory, { recursive: true });
+  console.log('-----Start dir deleted-----'.bgMagenta);
 };
 
-const replaceFiles = () => {
-  const names = fs.readdirSync(absolutePath(from));
-  const files = names.map(name => absolutePath(path.join(from, name)));
+const writeFile = (file, directory, encoding = 'base64') => {
+  fs.writeFileSync(directory, file, { encoding });
+  console.log(' file saved to', directory.red);
+};
+
+const isDirectory = (path) => {
+  const stat = fs.lstatSync(path);
+  return stat.isDirectory();
+};
+
+const replaceFiles = (startDir) => {
+  const names = fs.readdirSync(absolutePath(startDir));
+  const files = names.map(name => absolutePath(path.join(startDir, name)));
 
   files.forEach((file, npx) => {
     const name = names[npx];
+    const fileDir = path.join(startDir, name);
+
+    if (isDirectory(fileDir)) {
+      console.log('is directory'.bgRed);
+      return replaceFiles(fileDir);
+    }
+
     const firstLetter = name[0].toUpperCase();
     const directory = path.join(to, firstLetter, name);
     // Создает директорию с названием первой буквы файлы
@@ -36,9 +55,19 @@ const replaceFiles = () => {
 };
 
 const init = () => {
-  createDir([from, to]);
+  const isFromExist = isExist(from);
+  console.log('-----------Start-----------'.cyan);
 
-  replaceFiles();
+  if (!isFromExist) console.log(`${'Dir'.red} ${from.bgRed} ${"isn't exist".red}`);
+
+  if (isFromExist) {
+    createDir([from, to]);
+
+    replaceFiles(from);
+    if (remove) removeDir(from);
+  }
+
+  console.log('------------End------------'.cyan);
 };
 
 init();
