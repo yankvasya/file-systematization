6a6def6e40751281c2a8ce765087e27e684fa8ca
yankvasya <yankvasya@gmail.com>
2022-02-21 22:42:17 +0300

    feat: task completed

--- main.js
+++ main.js
@@ -1,81 +1,103 @@
 require('colors');
 
 const fs = require('fs');
-const fsPromises = fs.promises;
 const path = require('path');
 const { from, to, remove } = require('minimist')(process.argv.slice(2));
 
+const startApp = () => {
+  isExist(to, createDir(to, replaceFiles(from)));
+};
+
 const absolutePath = (name) => path.join(__dirname, name);
 
-const createDir = async (directory) => {
-  if (Array.isArray(directory)) return directory.forEach(d => createDir(d));
-
-  const isDirExist = await isExist(absolutePath(directory));
-
-  if (!isDirExist) await fsPromises.mkdir(directory);
+const createDir = (directory, callback) => {
+  fs.mkdir(directory, () => {
+    if (callback) callback();
+  });
 };
 
-const isExist = (file) => {
-  return fsPromises.access(file)
-    .then(() => true)
-    .catch(() => false);
-};
-
-const removeDir = async (directory) => {
-  await fsPromises.rmdir(directory, { recursive: true });
-  console.log(`-----${directory} dir deleted-----`.bgMagenta);
-};
-
-const writeFile = async (file, directory, encoding = 'base64') => {
-  await fsPromises.writeFile(directory, file, { encoding });
-  console.log(' file saved to', directory.red);
-};
-
-const isDirectory = async (path) => {
-  const stat = await fsPromises.lstat(path);
-  return stat.isDirectory();
-};
-
-const replaceFiles = async (startDir) => {
-  const names = await fsPromises.readdir(absolutePath(startDir));
-  const files = names.map(name => absolutePath(path.join(startDir, name)));
-
-  for (const file of files) {
-    const npx = files.indexOf(file);
-    const name = names[npx];
-    const fileDir = path.join(startDir, name);
-
-    if (!await isDirectory(fileDir)) {
-      const firstLetter = name[0].toUpperCase();
-      const directory = path.join(to, firstLetter, name);
-      // Создает директорию с названием первой буквы файлы
-      await createDir(path.join(to, firstLetter));
-
-      const data = await fsPromises.readFile(file, 'base64');
-      await writeFile(data, directory);
+const isExist = (file, callback, notCallback) => {
+  fs.access(absolutePath(file), (error) => {
+    if (!error) {
+      if (callback) callback();
     } else {
-      console.log(`${name.red} is directory`.bgRed);
-      await replaceFiles(fileDir);
+      console.log(`${'Dir'.red} ${file.bgRed} ${"isn't exist".red}`);
+      if (notCallback) {
+        notCallback();
+      }
     }
-  }
+  });
+};
+
+const removeDir = (directory) => {
+  fs.rmdir(directory, { recursive: true, force: true }, (error) => {
+    if (error) throw error;
+
+    console.log(`-----${directory} dir deleted-----`.bgMagenta);
+    process.exit(0);
+  });
+};
+
+const writeFile = (file, directory, encoding = 'base64') => {
+  fs.writeFile(directory, file, { encoding }, (error) => {
+    if (error) {
+      throw error;
+    } else {
+      console.log(' file saved to', directory.red);
+    }
+  });
+};
+
+const replaceFiles = (startDir) => {
+  fs.readdir(absolutePath(startDir), (error, data) => {
+    if (!error) {
+      const files = data.map(name => absolutePath(path.join(startDir, name)));
+
+      for (const file of files) {
+        const npx = files.indexOf(file);
+        const name = data[npx];
+        const fileDir = path.join(startDir, name);
+
+        fs.lstat(fileDir, (error, stats) => {
+          if (!error && !stats.isDirectory()) {
+            const firstLetter = name[0].toUpperCase();
+            const directory = path.join(to, firstLetter, name);
+            // Создает директорию с названием первой буквы файлы
+            createDir(path.join(to, firstLetter));
+            fs.readFile(file, 'base64', (imageError, imageData) => {
+              if (!imageError) {
+                writeFile(imageData, directory);
+              } else {
+                console.log(`Cannot read image - ${file.red}`.bgRed);
+              }
+            });
+          } else {
+            console.log(`${name.red} is directory`.bgRed);
+            replaceFiles(fileDir);
+          }
+        });
+      }
+    } else {
+      throw error;
+    }
+  });
 };
 
 const init = () => {
-  (async () => {
-    console.log('-----------Start----------- \n'.cyan);
-    const isFromExist = await isExist(from);
-
-    if (isFromExist) {
-      await createDir(to);
-      await replaceFiles(from);
-
-      if (remove) await removeDir(from);
-    } else {
-      console.log(`${'Dir'.red} ${from.bgRed} ${"isn't exist".red}`);
-    }
-
-    console.log('\n------------End------------'.cyan);
-  })();
+  console.log('-----------Start----------- \n'.cyan);
+  isExist(from, startApp);
 };
 
 init();
+
+process.on('beforeExit', () => {
+  if (remove) {
+    removeDir(from);
+  } else {
+    process.exit(0);
+  }
+});
+
+process.on('exit', () => {
+  console.log('\n------------End------------'.cyan);
+});
